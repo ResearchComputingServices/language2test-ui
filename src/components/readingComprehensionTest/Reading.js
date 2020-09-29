8779177921848faf12bf6f62cbb984fdbbb67c45
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2020-09-29 13:05:43 -0400

    Add .gitignore

--- src/components/readingComprehensionTest/Reading.js
+++ src/components/readingComprehensionTest/Reading.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Image from '../image';
@@ -6,16 +5,14 @@ import Image from '../image';
 const Reading = ({
     filename,
     text,
-    type,
 }) => (
     <>
-        {_.eq(type, 'image') && <Image imageName={filename} />}
-        {_.eq(type, 'text') && <p className='reading-comprehension-test-reading mb-3'>{text}</p>}
+        <p className='reading-comprehension-test-reading mb-3'>{text}</p>
+        {filename && <Image imageName={filename} />}
     </>
 );
 
 Reading.propTypes = {
-    type: PropTypes.string.isRequired,
     filename: PropTypes.string,
     text: PropTypes.string,
 };
