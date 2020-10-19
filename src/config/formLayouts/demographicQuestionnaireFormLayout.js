37a3863c980e2546c95f9b6bc87053df993e7080
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2020-10-19 10:47:04 -0400

    Fix demographic questionnaire css

--- src/config/formLayouts/demographicQuestionnaireFormLayout.js
+++ src/config/formLayouts/demographicQuestionnaireFormLayout.js
@@ -1,23 +1,29 @@
 export default [
     {
-        field: 'name',
-        title: 'Username',
-        type: 'text',
-        required: true,
-        disabled: true,
-    },
-    {
-        field: 'firstName',
-        title: 'First Name',
-        type: 'text',
-        required: true,
-        disabled: true,
-    },
-    {
-        field: 'lastName',
-        title: 'Last Name',
-        type: 'text',
-        required: true,
-        disabled: true,
+        type: 'section',
+        className: 'form-row justify-content-center',
+        elements: [
+            {
+                field: 'name',
+                title: 'Username',
+                type: 'text',
+                required: true,
+                disabled: true,
+            },
+            {
+                field: 'firstName',
+                title: 'First Name',
+                type: 'text',
+                required: true,
+                disabled: true,
+            },
+            {
+                field: 'lastName',
+                title: 'Last Name',
+                type: 'text',
+                required: true,
+                disabled: true,
+            },
+        ],
     },
 ];
