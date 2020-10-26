1a8640a18e07bb0ca9c3e73ec29bc1dc6387b6ff
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2020-10-26 11:41:05 -0400

    Add Welcome Component for dashboard

--- src/components/administratorDashboard/index.js
+++ src/components/administratorDashboard/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useStore } from '../../hooks';
+import Welcome from './Welcome';
 
 function AdministratorDashboard() {
-    return <div>Hello World</div>;
+    const userSession = useStore('userSession');
+    return (
+        <>
+            <Welcome name={userSession.firstName} />
+        </>
+    );
 }
 
 export default AdministratorDashboard;
