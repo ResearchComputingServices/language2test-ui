976c48788d8e7908951afa42fb3b39d4f895ded4
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2020-10-27 12:17:33 -0400

    Remove AuthorizationChecker from existence

--- src/hooks/useFormButtons.js
+++ src/hooks/useFormButtons.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import useRolesCheckerService from './useRolesCheckerService';
 
-function getButton(authorizationCheckerService, title, handler, options = {}) {
+function getButton(rolesCheckerService, title, handler, options = {}) {
     const skipAuthorization = _.get(options, 'skipAuthorization', false);
     const authorization = _.get(options, 'authorization', null);
-    const hasAuthorization = skipAuthorization ? true : (authorization ? (authorizationCheckerService.has(authorization)) : true);
+    const hasAuthorization = skipAuthorization ? true : (authorization ? (rolesCheckerService.has(authorization)) : true);
     if (_.isBoolean(handler) && !handler) {
         return {
             title,
@@ -24,26 +24,26 @@ function getButton(authorizationCheckerService, title, handler, options = {}) {
 }
 
 export default function useFormButtons(id, entity, handlers, disableMutation = false, disableDelete = false, skipAuthorization = false) {
-    const authorizationCheckerService = useRolesCheckerService();
+    const rolesCheckerService = useRolesCheckerService();
     const { create, update, remove, download, cancel } = handlers;
     entity = _.kebabCase(entity);
     return [
-        _.isNil(id) ? getButton(authorizationCheckerService, 'Create', create, {
+        _.isNil(id) ? getButton(rolesCheckerService, 'Create', create, {
             disabled: disableMutation,
             authorization: `create-${entity}`,
             skipAuthorization,
         }) : null,
-        _.isNil(id) ? null : getButton(authorizationCheckerService, 'Update', update, {
+        _.isNil(id) ? null : getButton(rolesCheckerService, 'Update', update, {
             disabled: disableMutation,
             authorization: `update-${entity}`,
             skipAuthorization,
         }),
-        _.isNil(id) ? null : getButton(authorizationCheckerService, 'Delete', remove, {
+        _.isNil(id) ? null : getButton(rolesCheckerService, 'Delete', remove, {
             disabled: disableDelete,
             authorization: `delete-${entity}`,
             skipAuthorization,
         }),
-        _.isNil(id) ? null : getButton(authorizationCheckerService, 'Export', download, { authorization: `export-${entity}` }),
-        getButton(authorizationCheckerService, 'Cancel', cancel, { type: 'utility' }),
+        _.isNil(id) ? null : getButton(rolesCheckerService, 'Export', download, { authorization: `export-${entity}` }),
+        getButton(rolesCheckerService, 'Cancel', cancel, { type: 'utility' }),
     ];
 }
